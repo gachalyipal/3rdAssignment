cb608b94234c0265dbb26e772f6636d02a8e2c28
gachalyipal <50751752+gachalyipal@users.noreply.github.com>
2019-05-19 12:59:49 +0200

    Add files via upload

---
+++ Button.hpp
@@ -0,0 +1,27 @@
+#ifndef BUTTON_HPP_INCLUDED
+#define BUTTON_HPP_INCLUDED
+
+#include "widgets.hpp"
+#include <string>
+#include <functional>
+using namespace std;
+
+class Button : public Widget {
+protected:
+    function <void()> funk;
+    string str;
+    bool _focused;
+    bool on_clicked;
+public:
+    Button(int x, int y, int sx, int sy, string text, function <void()>_funk);
+    virtual void draw();
+    virtual void handle(genv::event ev);
+    virtual bool is_focused();
+    virtual string get_value(){return str;};
+    virtual void set_click();
+    virtual bool get_click();
+};
+
+
+
+#endif // BUTTON_HPP_INCLUDED
---
+++ button.cpp
@@ -0,0 +1,57 @@
+#include "Button.hpp"
+#include <functional>
+#include <iostream>
+
+using namespace genv;
+
+Button::Button(int x, int y, int sx, int sy,  string text, function <void()>_funk)
+    : Widget(x,y,sx,sy)
+{
+    funk=_funk;
+    str=text;
+    _focused=false;
+    on_clicked=false;
+}
+void Button::draw()
+{
+    if (_focused)
+    {
+        gout << move_to(_x, _y) << color(255,0,0) << box(_size_x, _size_y);
+        gout << move_to(_x+2, _y+2) << color(0,0,0) << box(_size_x-4, _size_y-4);
+    }
+    else
+    {
+        gout << move_to(_x, _y) << color(255,255,255) << box(_size_x, _size_y);
+        gout << move_to(_x+2, _y+2) << color(0,0,0) << box(_size_x-4, _size_y-4);
+    }
+        gout << color(255,255,255)<<move_to(_x+20,_y+28)<< text(str);
+}
+
+void Button::handle(event ev)
+{
+
+    if (ev.type == ev_mouse && ev.button==btn_left)
+    {
+        if ( is_selected(ev.pos_x, ev.pos_y) )
+        {
+            on_clicked=true;
+            _focused=true;
+            funk();
+        }
+        else
+        {
+            _focused=false;
+        }
+    }
+
+}
+bool Button::is_focused()
+{
+    return _focused;
+}
+void Button:: set_click(){
+    on_clicked=false;
+}
+bool Button:: get_click(){
+    return on_clicked;
+}
